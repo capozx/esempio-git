c75cd88307a019d902c9362985c31557b34448bb
Dario Capozzi <me@dariocapozzi.it>
2025-09-05 08:59:27 +0200

    implementing spinning-cat.component

---
+++ src/app/spinning-cat/spinning-cat.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'spinning-cat',
+  standalone: true,
+  imports: [],
+  templateUrl: './spinning-cat.component.html',
+  styleUrl: './spinning-cat.component.scss'
+})
+export class SpinningCatComponent {
+
+}
